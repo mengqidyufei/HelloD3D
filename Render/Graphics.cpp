ddcd5b13658d82bdc5a7d82ef8d4bdaae3922c20
Cyfei <mengqi_d_yufei@163.com>
2024-07-23 21:15:12 +0800

    VertexIndexBuffer

--- Render/Graphics.cpp
+++ Render/Graphics.cpp
@@ -95,33 +95,76 @@ void Graphics::drawTriangle()
 		{ 0.0f, 0.5f, 255, 0, 0 },
 		{ 0.5f, -0.5f, 0, 255, 0 },
 		{ -0.5f, -0.5f, 0, 0, 255 },
+
+		//{ 0.0f, 0.5f, 255, 0, 0 },
+		//{ -0.5f, -0.5f, 0, 255, 0 },
+		{ -0.3f, 0.3f, 0, 0, 255 },
+
+		//{ 0.0f, 0.5f, 255, 0, 0 },
+		{ 0.3f, 0.3f, 0, 255, 0 },
+		//{ 0.5f, -0.5f, 0, 0, 255 },
+
+		{ 0.0f, -0.8f, 255, 0, 0 },
+		//{ -0.5f, -0.5f, 0, 255, 0 },
+		//{ 0.5f, -0.5f, 0, 0, 255 },
 	};
-	// 描述Buffer是干什么用的
-	D3D11_BUFFER_DESC desc;				
-	desc.ByteWidth = sizeof(vertices);
-	desc.Usage = D3D11_USAGE_DEFAULT;
-	desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-	desc.CPUAccessFlags = 0u;
-	desc.MiscFlags = 0u;
-	desc.StructureByteStride = sizeof(Vertex);
+	{
+		// 描述Buffer是干什么用的
+		D3D11_BUFFER_DESC descVertex;				
+		descVertex.ByteWidth = sizeof(vertices);
+		descVertex.Usage = D3D11_USAGE_DEFAULT;
+		descVertex.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		descVertex.CPUAccessFlags = 0u;
+		descVertex.MiscFlags = 0u;
+		descVertex.StructureByteStride = sizeof(Vertex);
+
+		// 真正输入给Buffer的数据
+		D3D11_SUBRESOURCE_DATA subDataVertex;		
+		subDataVertex.pSysMem = vertices;
+		subDataVertex.SysMemPitch = 0u;
+		subDataVertex.SysMemSlicePitch = 0u;
+
+		// 创建ID3D11Buffer
+		wrl::ComPtr<ID3D11Buffer> vertexBuffer;
+		HRESULT hr;
+		GFX_THROW_INFO( mDevice->CreateBuffer(&descVertex, &subDataVertex, &vertexBuffer) );
+
+		// 设置输入装配器
+		UINT pStrides = sizeof(Vertex);
+		UINT offset = 0u;
+		// 此处用GetAddressOf是因为，此处我们不需要给vertexBuffer填充任何东西，只是指定指针地址，
+		// 而ComPtr用&符的话就会把之前的内容释放掉，不是我们想要的结果，所以用GetAdressOf仅仅只取地址
+		mContext->IASetVertexBuffers(0u, 1u, vertexBuffer.GetAddressOf(), &pStrides, &offset);
+	}
+
+	// 顶点索引
+	const unsigned short indices[] =
+	{
+		0, 1, 2,
+		0, 2, 3,
+		0, 4, 1,
+		5, 2, 1,
+	};
+	// 设置Buffer类型为顶点索引，以及步进量
+	D3D11_BUFFER_DESC descIndexBuffer = {};
+	descIndexBuffer.ByteWidth = sizeof(indices);
+	descIndexBuffer.Usage = D3D11_USAGE_DEFAULT;
+	descIndexBuffer.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	descIndexBuffer.CPUAccessFlags = 0u;
+	descIndexBuffer.MiscFlags = 0u;
+	descIndexBuffer.StructureByteStride = sizeof(unsigned short);
 
 	// 真正输入给Buffer的数据
-	D3D11_SUBRESOURCE_DATA subData;		
-	subData.pSysMem = vertices;
-	subData.SysMemPitch = 0u;
-	subData.SysMemSlicePitch = 0u;
+	D3D11_SUBRESOURCE_DATA subDataIndexBuffer = {};
+	subDataIndexBuffer.pSysMem = indices;
+	subDataIndexBuffer.SysMemPitch = 0u;
+	subDataIndexBuffer.SysMemSlicePitch = 0u;
 
 	// 创建ID3D11Buffer
-	wrl::ComPtr<ID3D11Buffer> vertexBuffer;
+	wrl::ComPtr<ID3D11Buffer> vertexIndexBuffer;
 	HRESULT hr;
-	GFX_THROW_INFO( mDevice->CreateBuffer(&desc, &subData, &vertexBuffer) );
-
-	// 设置输入装配器
-	UINT pStrides = sizeof(Vertex);
-	UINT offset = 0u;
-	// 此处用GetAddressOf是因为，此处我们不需要给vertexBuffer填充任何东西，只是指定指针地址，
-	// 而ComPtr用&符的话就会把之前的内容释放掉，不是我们想要的结果，所以用GetAdressOf仅仅只取地址
-	mContext->IASetVertexBuffers(0u, 1u, vertexBuffer.GetAddressOf(), &pStrides, &offset);
+	GFX_THROW_INFO(mDevice->CreateBuffer(&descIndexBuffer, &subDataIndexBuffer, &vertexIndexBuffer));
+	mContext->IASetIndexBuffer(vertexIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
 
 	// 加载VertexShader
 	wrl::ComPtr<ID3DBlob> blobVS;
@@ -167,6 +210,9 @@ void Graphics::drawTriangle()
 	viewport.MinDepth = 0;
 	viewport.MaxDepth = 1;
 	mContext->RSSetViewports(1u, &viewport);
+
 	//GFX_THROW_INFO_ONLY(mContext->Draw(3u, 0u));
-	mContext->Draw((UINT)std::size(vertices), 0u);
+	//mContext->Draw((UINT)std::size(vertices), 0u);
+
+	mContext->DrawIndexed((UINT)std::size(indices), 0u, 0u);
 }
