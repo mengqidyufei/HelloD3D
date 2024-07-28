764981a59f5aa755ca5da9c968ab498406663ef1
Cyfei <mengqi_d_yufei@163.com>
2024-07-28 20:30:56 +0800

    Only draw Box

--- App/App.cpp
+++ App/App.cpp
@@ -21,21 +21,21 @@ App::App()
 		{
 			switch (typedist(rng))
 			{
-			case 0:
-				return std::make_unique<Pyramid>(
-					gfx, rng, adist, ddist,
-					odist, rdist
-					);
+				//case 0:
+				//	return std::make_unique<Pyramid>(
+				//		gfx, rng, adist, ddist,
+				//		odist, rdist
+				//		);
 			case 1:
 				return std::make_unique<Box>(
 					gfx, rng, adist, ddist,
 					odist, rdist, bdist
 					);
-			case 2:
-				return std::make_unique<Melon>(
-					gfx, rng, adist, ddist,
-					odist, rdist, longdist, latdist
-					);
+				//case 2:
+				//	return std::make_unique<Melon>(
+				//		gfx, rng, adist, ddist,
+				//		odist, rdist, longdist, latdist
+				//		);
 			default:
 				assert(false && "bad drawable type in factory");
 				return {};
@@ -51,7 +51,7 @@ App::App()
 		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
 		std::uniform_int_distribution<int> latdist{ 5,20 };
 		std::uniform_int_distribution<int> longdist{ 10,40 };
-		std::uniform_int_distribution<int> typedist{ 0,2 };
+		std::uniform_int_distribution<int> typedist{ 1,1 };
 	};
 
 	Factory f(mWnd.getGraphics());
